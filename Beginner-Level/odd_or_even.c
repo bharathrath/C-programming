5d1b3c77440b1968932428f0ba303c01b3cbbfd4
bharathrath <38652651+bharathrath@users.noreply.github.com>
2018-04-24 09:41:53 +0000

    Update odd_or_even.c

--- Beginner-Level/odd_or_even.c
+++ Beginner-Level/odd_or_even.c
@@ -3,13 +3,20 @@ int main()
 {
 int n;
 scanf("%d",&n);
-if(n%2==0)
+if(n>0 && n<100000)
 {
-printf("Even\n");
+ if(n%2==0)
+ {
+  printf("Even\n");
+ }
+ else
+ {
+  printf("Odd\n");
+ }
 }
 else
 {
-printf("Odd\n");
-}
+ printf("Enter number below 100000\n");
+}  
 return 0;
 }
