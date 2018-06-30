8827157fee9097d9fdab01dd43f4b86e781a8935
bharathrath <38652651+bharathrath@users.noreply.github.com>
2018-06-30 06:48:50 +0000

    Update leap.c

--- Beginner-Level/leap.c
+++ Beginner-Level/leap.c
@@ -5,17 +5,17 @@ int year;
 scanf("%d",&year);
 if(year%4 == 0)
 {
-if( year%100 == 0)
-{
-if ( year%400 == 0)
-printf("yes");
-else
-printf("No");
+  if( year%100 == 0)
+  { 
+    if ( year%400 == 0)
+      printf("yes");
+    else
+      printf("no");
 }
 else
-printf("yes");
+  printf("yes");
 }
 else
-printf("No");
+  printf("no");
 return 0;
 }
