52b9aa488af0d4da787b56b8822a0857fb61d3f7
bharathrath <38652651+bharathrath@users.noreply.github.com>
2018-06-30 07:08:23 +0000

    Create power.c

---
+++ Beginner-Level/power.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,b,i;
+    int sum=1;
+    scanf("%d%d",&n,&b);
+    for(i=0;i<b;i++)
+    {
+        sum=sum*n;
+    }
+    printf("%d",sum);
+   return 0;
+}
